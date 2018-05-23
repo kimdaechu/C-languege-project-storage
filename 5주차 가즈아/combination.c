43d8dc4b888d105984e8b390ac4996f5114a888a
kimdaehyun <redfox6514@gmail.com>
2018-05-23 23:21:40 +0900

    5주차 가즈아~~

---
+++ 5주차 가즈아/combination.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int get_integer(void);
+int combination(int n, int r);
+long factorial(int n);
+
+int main(void)
+{
+	int a, b;
+	int get_integer(void);
+
+	a = get_integer();
+	b = get_integer();
+
+	printf("C(%d,%d)=%d \n", a, b, combination(a, b));
+	return 0;
+}
+
+int combination(int n, int r)
+{
+	return (factorial(n) / (factorial(r)*factorial(n - r)));
+}
+int get_integer(void)
+{
+	int n;
+
+	printf("정수를 입력하시오: ");
+	scanf_s("%d", &n);
+
+	return n;
+}
+
+long factorial(int n)
+{
+	int i;
+	long result = 1;
+	
+	for (i = 1; i <= n; i++)
+		result *= i;
+
+	return result;
+}
